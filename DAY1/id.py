2c13a746ae27daa11d6b07d2291073123310d4df
rohitrepo1 <rohitsundik@outlook.com>
2022-03-14 10:14:06 +0000

    adding new files

---
+++ DAY1/id.py
@@ -0,0 +1,9 @@
+#its a pre defined  function to know the address of the variable or identifier  where our vaules are stored##
+import time
+age=25
+print("the age is:",age)
+print()
+print("the address of the age is:",id(age))
+print()
+time.sleep(3)
+print("end of application")
---
+++ DAY1/index.py
@@ -0,0 +1,9 @@
+import time
+print("test")
+print()
+print('test')
+print()
+print("""test""")
+print()
+time.sleep(5)
+print("end of application")
