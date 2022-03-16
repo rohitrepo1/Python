afc28e7f83cba4ee13ac5f0e330b1a44608cff7e
rohitrepo1 <rohitsundik@outlook.com>
2022-03-16 11:10:21 +0000

    DAY3

---
+++ DAY3/float.py
@@ -0,0 +1,17 @@
+import time
+Y1=123.123
+print("the vaule is:",Y1)
+print("the data type is:",type(Y1))
+Z2=-123.123
+print("the vaules is:",Z2)
+time.sleep(2)
+print("the data type is:",type(Z2))
+print()
+print("END OF PROGRAME")
+print()
+##########################################
+#exponential vaules 
+X_1=1.3*3*10
+print("the value is:",X_1)
+print("the data type is:",type(X_1))
+
---
+++ DAY3/sliceoperator.py
@@ -0,0 +1,13 @@
+#python supports slice operator , the main objective of slice operator is to make  pieces of the string  as per the business login based on the condition 
+#***str1[begin:end-1:step]***#
+#Slice operator  with positive index
+import time
+print("EXAMPLE1")
+str1="Learn Python"
+print(str1)
+print()
+print("type of data is:",type(str1))
+print("slice operator with positive index")
+print(str1[1:])
+#############################################
+print(str1[:5])
