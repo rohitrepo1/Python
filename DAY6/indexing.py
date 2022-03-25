3418b7548760a6ceb0945d022947ae33892ec01f
rohitrepo1 <rohitsundik@outlook.com>
2022-03-25 05:44:18 +0000

    day6

---
+++ DAY6/indexing.py
@@ -0,0 +1,5 @@
+import time
+product_info=[1001,"mobile","Samsung","25-03-2022"]
+print()
+print("The product PID is:",product_info[0])
+print("The Product Type is:",product_info[1])
---
+++ DAY6/tuple.py
@@ -0,0 +1,12 @@
+from cgi import test
+import time
+test1=()
+print(type(test1))
+print(test1)
+test2=tuple()
+print(type(test2))
+print(test2)
+#example2
+demo=(10,)
+print(type(demo))
+print(demo)
